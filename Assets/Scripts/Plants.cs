4ef96144c03dc3af7b3273b694433446b66b181a
thaDameer <38245328+thaDameer@users.noreply.github.com>
2020-07-11 11:07:50 +0200

    Weed functionality

---
+++ Assets/Scripts/Plants.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plants : MonoBehaviour
+{
+    public enum PlantLevel
+    {
+        DIRTPILE,
+        BUSH,
+        MONSTER
+    }
+    [Header("Plant Level Objects")]
+    public GameObject plantLvl1, plantLvl2, plantLvl3; 
+    public PlantLevel plantLevel;
+    public int health  = 1;
+    public Animator animator;
+    public float spawnInterval = 5;
+    public TimerSimple spawnTimer;
+    public ParticleSystem ps;
+    bool hasSpawned = false;
+
+   
+    private void Start()
+    {
+        SpawnPlant(5);
+    }
+
+    public void SpawnPlant(float spawnInterval)
+    {
+        if (hasSpawned) return;
+        this.spawnInterval = spawnInterval;
+        plantLvl1.SetActive(true);
+        spawnTimer = new TimerSimple(spawnInterval);
+        plantLevel = PlantLevel.DIRTPILE;
+        ps.Emit(10);
+        StartCoroutine(PlantGrowing_CO());
+        hasSpawned = true;
+    }
+
+    IEnumerator PlantGrowing_CO()
+    {
+        spawnTimer.Start();
+        while (spawnTimer.isRunning)
+        {
+            if (spawnTimer.isTimerElapsed) spawnTimer.Stop();
+            yield return new WaitForEndOfFrame();
+        }
+        LevelUp();
+    }
+
+    public virtual void Update()
+    {
+        if (spawnTimer.isTimerElapsed)
+        {
+            LevelUp();
+        }
+    }
+
+    public void LevelUp()
+    {
+        switch (plantLevel)
+        {
+            case PlantLevel.DIRTPILE:
+                ps.Emit(10);
+                plantLvl1.SetActive(false);
+                plantLvl2.SetActive(true);
+                plantLvl3.SetActive(false);
+                StartCoroutine(PlantGrowing_CO());
+                plantLevel = PlantLevel.BUSH;
+                break;
+            case PlantLevel.BUSH:
+                ps.Emit(10);
+                plantLvl1.SetActive(false);
+                plantLvl2.SetActive(false);
+                plantLvl3.SetActive(true);
+                StartCoroutine(PlantGrowing_CO());
+                plantLevel = PlantLevel.MONSTER;
+                break;
+            case PlantLevel.MONSTER:
+                ps.Emit(100);
+                break;
+        }
+    }
+    
+
+    public virtual void UpdateHealth()
+    {
+        switch (plantLevel)
+        {
+            case PlantLevel.DIRTPILE:
+                health = 1;
+                break;
+            case PlantLevel.BUSH:
+                health = 2;
+                break;
+            case PlantLevel.MONSTER:
+                health = 3;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/TimerSimple.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+
+// This is a struct so we can create arrays of timers without constructing each of them
+public struct TimerSimple
+{
+    public float lifespan;
+    public bool unscaled;
+    private float timerLastUpdate;
+
+    /// <summary>
+    /// Returns true if the timer is running; even if it has elapsed.
+    /// For example, if the timer has not been stopped (via Stop()), 
+    /// and the timer has elapsed, this will still return true. 
+    /// </summary>
+    public bool isRunning { get; private set; }
+
+    private float timer;
+
+    public TimerSimple(float lifespan = 1f, bool unscaled = false)
+    {
+        this.lifespan = lifespan;
+        this.unscaled = unscaled;
+        this.isRunning = false;
+        timer = 0;
+        timerLastUpdate = 0;
+    }
+
+    public float elapsedTime
+    {
+        get
+        {
+            Update();
+            return timer;
+        }
+    }
+
+    public float remainingTime => lifespan - elapsedTime; 
+
+    /// <summary>
+    /// Is true IFF the timer has elapsed.
+    /// </summary>
+    public bool isTimerElapsed => elapsedTime >= lifespan;
+
+    public float elapsedRatio
+    {
+        get
+        {
+            Debug.Assert(lifespan > float.Epsilon, "lifespan too short to compute ratio");
+            return elapsedTime / lifespan;
+        }
+    }
+
+    public float invElapsedRatio => 1f - elapsedRatio;
+
+    public float elapsedRatioLooping => Mathf.Repeat(elapsedRatio, 1f);
+
+    public float elapsedRatioLoopingPingPong
+    {
+        get
+        {
+            var t = Mathf.Repeat(elapsedRatio, 1f);
+            return 2f * (t < .5f ? t : 1f - t);
+        }
+    }
+
+    private void Update()
+    {
+        var currentTime = unscaled ? Time.unscaledTime : Time.time;
+        if (isRunning)
+        {
+            timer += currentTime - timerLastUpdate;
+        }
+        timerLastUpdate = currentTime;
+    }
+
+    public void Stop()
+    {
+        isRunning = false;
+    }
+
+    public void Start(bool reset = true)
+    {
+        if (reset)
+        {
+            // When resetting, set 'running' to false to prevent the first
+            // Update from reusing the previous timerLastUpdate.
+            isRunning = false;
+
+            timer = 0f;
+        }
+        Update();
+        isRunning = true;
+    }
+
+    public void Start(float newLifespan, bool reset = true)
+    {
+        lifespan = newLifespan;
+        Start(reset);
+    }
+
+    public void DelayedStart(float delay)
+    {
+        isRunning = false;
+        timer = -delay;
+        Update();
+        isRunning = true;
+    }
+
+    public void FastForward(float x)
+    {
+        timerLastUpdate -= x;
+    }
+
+    public static TimerSimple StartNew(float lifespan = 1f, bool unscaled = false)
+    {
+        var t = new TimerSimple(lifespan, unscaled);
+        t.Start();
+        return t;
+    }
+}
